20f4ace59e893c71659331fd9cb25071e86dd6ba
rs-tungdh <tungdh@runsystem.net>
2024-01-04 09:56:50 +0700

    create project

---
+++ java-spring-boot-study/src/main/java/org/example/javaspringbootstudy/JavaSpringBootStudyApplication.java
@@ -0,0 +1,13 @@
+package org.example.javaspringbootstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringBootStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaSpringBootStudyApplication.class, args);
+    }
+
+}
---
+++ java-spring-boot-study/src/test/java/org/example/javaspringbootstudy/JavaSpringBootStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.javaspringbootstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringBootStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
